fbbdbcaf27feea0ddbd32d427097ca0f7425ea7f
tabowink-glitch <tabowink@gmail.com>
2026-08-02 01:01:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ btn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+// Membuat love putih
+setInterval(() => {
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "🤍";
+
+    heart.style.position = "fixed";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.top = "-20px";
+    heart.style.fontSize = (20 + Math.random() * 20) + "px";
+    heart.style.pointerEvents = "none";
+    heart.style.animation = "fall 6s linear forwards";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    },6000);
+
+},800);
